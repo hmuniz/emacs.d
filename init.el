faa2f6bd936dffc3f0281380a13e31d757f1f464
Henrique Muniz <hn.muniz.a@gmail.com>
2022-02-04 11:45:28 -0300

    Update init.el

--- init.el
+++ init.el
@@ -1,2 +1,15 @@
 ;;; My emacs config
 ;;; Code:
+
+;;; My emacs config
+;;; Code:
+(show-paren-mode 1)
+(global-hl-line-mode 1)
+(column-number-mode)
+
+(tool-bar-mode -1)
+
+(menu-bar-mode -1)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
